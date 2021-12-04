e0bd32abd77cdc1936af7d4c5bee4d5d9788ec91
Kevin Muldoon <kmuldoon@mac.com>
2021-12-04 09:46:05 -0500

    Working

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,71 @@
+// Firebase tutorial
+// https://www.youtube.com/watch?v=zQyrwxMPm88
+
+// Dexie Database
+// https://dexie.org/docs/Typescript
+
+
+// https://codetain.com/blog/dictionary-in-typescript
+
+
 // https://www.fullstacklabs.co/blog/overload-typescript-react-component-interfaces-prop-values
 
+// https://www.codegrepper.com/code-examples/javascript/frameworks/express/typescript+local+database
 
-import React from 'react';
+// https://github.com/pouchdb/pouchdb
+
+// https://programmingwithmosh.com/react/localstorage-react/
+
+// https://github.com/marcuswestin/store.js/
+
+// https://coderwall.com/p/ewxn9g/storing-and-retrieving-objects-with-localstorage-html5
+
 import './App.css';
+import { NavBar } from "./NavBar"
+import { SwatchColumnLegend } from "./SwatchColumnLegend"
 import { SwatchColumn }  from "./SwatchColumn";
 
 function App() {
 
+    // localStorage.clear();
+    // https://developer.microsoft.com/en-us/fluentui#/styles/web/colors/products
+
     return (
         <div className="App">
-            <SwatchColumn model={{hex:"#0000FF", semantic: "primary"}}/>
-            <SwatchColumn model={{hex:"#cc0000", semantic: "secondary"}}/>
-            <SwatchColumn model={{hex:"#118437", semantic: "success"}}/>
-            <SwatchColumn model={{hex:"#116EDF", semantic: "info"}}/>
+            <NavBar/>
+            <SwatchColumnLegend/>
+
+            {/* IBM */}
+            <SwatchColumn model={{hex:"#082B9F", semantic: "primary"}} />
+            <SwatchColumn model={{hex:"#1057F7", semantic: "secondary"}}/>
+            <SwatchColumn model={{hex:"#198038", semantic: "success"}}/>
+            <SwatchColumn model={{hex:"#8a3ffc", semantic: "info"}}/>
+            <SwatchColumn model={{hex:"#FFB000", semantic: "warning"}}/>
+            <SwatchColumn model={{hex:"#da1e28", semantic: "danger"}}/>
+            <SwatchColumn model={{hex:"#6f6f6f", semantic: "neutral"}}/>
+
+
+            {/* // GREENS */}
+            {/* <SwatchColumn model={{hex:"#179937", semantic: "primary"}} />
+            <SwatchColumn model={{hex:"#009925", semantic: "secondary"}}/>
+            <SwatchColumn model={{hex:"#0e9931", semantic: "info"}}/>
+            <SwatchColumn model={{hex:"#118236", semantic: "success"}}/>
+            <SwatchColumn model={{hex:"#00704A", semantic: "warning"}}/>
+            <SwatchColumn model={{hex:"#D90303", semantic: "danger"}}/>
+            <SwatchColumn model={{hex:"#6E6E6E", semantic: "neutral"}}/> */}
+
+            {/* GENOME */}
+            {/* <SwatchColumn model={{hex:"#9C1DEB", semantic: "primary"}} />
+            <SwatchColumn model={{hex:"#009925", semantic: "secondary"}}/>
+            <SwatchColumn model={{hex:"#118236", semantic: "success"}}/>
+            <SwatchColumn model={{hex:"#1369D0", semantic: "info"}}/>
             <SwatchColumn model={{hex:"#FFC107", semantic: "warning"}}/>
-            <SwatchColumn model={{hex:"#6E6E6E", semantic: "neutral"}}/>
+            <SwatchColumn model={{hex:"#D90303", semantic: "danger"}}/>
+            <SwatchColumn model={{hex:"#6E6E6E", semantic: "neutral"}}/> */}
         </div>
     );
 }
 
+// 179937
+
 export default App;
-
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/Swatch.tsx
+++ src/Swatch.tsx
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
-import { SwatchModel } from './SwatchModel'
+import { SwatchModel } from './models'
 
 export const Swatch: React.FC<SwatchModel> = (model: SwatchModel) => {
 
+    let label = "n/a"
+    // Do not write 000 or 950 weights if semantic name != 'neutral'
+    if (!(model.semantic != 'neutral' && (model.weight == '000' || model.weight == '950'))) {
+        localStorage.setItem(model.name, model.hex)
+        label = model.hex
+    }
+
     function onClickHandler() {
         console.table(model)
+        // let foo = prompt('Type here');
+        // let bar = confirm('Confirm or deny');
+        // console.log(foo, bar);
+    }
+
+    function isOK() {
+        if (model.weight === "400" && model.LAB.L > 47) {
+            console.log("EHHIHDFHIDHF")
+            console.table(model)
+
+        }
     }
 
     const wrapper = {
@@ -15,13 +33,15 @@ export const Swatch: React.FC<SwatchModel> = (model: SwatchModel) => {
         width: '100%',
         background: model.hex,
         height: '48px',
-        color: (model.LAB.L < 51 ? '#FFFFFF' : '#000000')
+        color: (model.LAB.L < 51 ? '#FFFFFF' : '#000000'),
     };
+
+    isOK()
 
     return (
         <div key={model.name} style={wrapper as React.CSSProperties}
             onClick={() => onClickHandler()}>
-            {model.hex}
+            {label}
         </div>
     )
 
