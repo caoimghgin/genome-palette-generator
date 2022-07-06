b25f98d2c2422b942a2d36cf8085dab9662439d9
Kevin Muldoon <kmuldoon@mac.com>
2022-07-06 19:55:27 -0400

    Update Footer.tsx
    Sooooo close to copy perfection.

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -74,9 +74,9 @@ const StyledContent = styled.p`
                     <b>Genome Color Space</b> is an <b>Open-Source Color Tool for Color Systems.</b> It was created to be <b>Simple, Effective, Universal, Extensible,</b> and <b>WCAG Compliant</b>. 
                    Its purpose is to help designers create brand-specific Color Systems from existing style-guides and easily facilitate white-labeling at scale.
                    
-                   <p> <b>Genome is Color System Agnostic</b>, allowing the user to choose <b>Material Design</b>, <b>IBM Carbon</b>, <b>SalesForce</b>, <b>Lyft</b>, and many more. In fact, you could make your own if you wanted.</p>
+                   <p> <b>Genome is Color System Agnostic</b>, allowing the user to choose <b>Material Design</b>, <b>IBM Carbon</b>, <b>SalesForce</b>, <b>Lyft</b>, and many more. In fact, you could make your own.</p>
  
-                   <p>To import the results of this tool into your design application,  <b>you'll want to download a plugin.</b> Today, a Figma plugin is available but I'll write for AdobeXD and even Sketch in the future. </p>
+                   <p>To import the results of this tool into your design application, <b>you'll want to download a plugin.</b> Today, a Figma plugin is available but I'll write for AdobeXD and even Sketch in the future. </p>
 
                    <p><b>DISCLAIMER:</b> This Color Tool<b> IS NOT AFFILIATED</b> with any Design System or organization. Unless otherwise noted, all results are the <b>research</b> and <b>opinions </b> of myself.
                    </p>
