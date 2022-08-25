f4544030ab6765077ef98bdc7644f75c879df842
Kevin Muldoon <kmuldoon@mac.com>
2022-08-25 06:09:07 -0400

    Update ResourcesView.tsx
    Added link to Figma plugin. Cleanups

--- src/components/ResourcesView.tsx
+++ src/components/ResourcesView.tsx
@@ -1,21 +1,13 @@
 import React, { useState, useEffect } from 'react';
-// import styled from '@emotion/styled/macro'
 import styled from '@emotion/styled'
 import logoUXCollectiveImage from './../resources/logo-uxcollective.png';
 import pluginImage from './../resources/plugin.png';
-import openSourceImage from './../resources/openSourceImage.png';
 import linkedInImage from './../resources/linkedInImage.png';
 import twitterLogoImage from './../resources/twitterLogoImage.png';
 import portfolioImage from './../resources/portfolioImage.png';
 import githubLogoImage from './../resources/githubLogoImage.png';
 
 export const ResourcesView: React.FC = () => {
-
-    const handleOnClick = (event: React.MouseEvent<HTMLInputElement>) => {
-       let value = event.currentTarget.value
-        event.preventDefault();
-        window.open(value);
-    }
 
     const ModalContainer = styled.div`
         width: 360px;
@@ -26,88 +18,69 @@ export const ResourcesView: React.FC = () => {
         padding: 4px 24px 24px 24px;
     `
 
-    //     How should you name your colors in a Design System?
-    // Definitive, Semantic, and Contextual naming conventions
-
     return (
         <ModalContainer>
 
             <h3> Articles </h3>
-            <hr/>
-            <ResourcesViewTabelCell 
-                title={"The Genome Color Tool (2022)"} 
+            <hr />
+            <ResourcesViewTabelCell
+                title={"The Genome Color Tool (2022)"}
                 link={"https://uxdesign.cc/the-genome-color-tool-28ce73b20768"}
                 icon={logoUXCollectiveImage}
             />
 
-            <ResourcesViewTabelCell 
-                title={"How to name your colors in a Design System (2022)"} 
+            <ResourcesViewTabelCell
+                title={"How to name your colors in a Design System (2022)"}
                 link={"https://medium.com/user-experience-design-1/how-should-you-name-your-colors-in-a-design-system-3086513476df"}
                 icon={logoUXCollectiveImage}
             />
 
-            <ResourcesViewTabelCell 
-                title={"Applying game design logic to your design system (2021)"} 
+            <ResourcesViewTabelCell
+                title={"Applying game design logic to your design system (2021)"}
                 link={"https://uxdesign.cc/applying-game-design-logic-to-your-design-system-111a2116509"}
                 icon={logoUXCollectiveImage}
 
-            />            
+            />
 
-            {/* <h3> Plugins </h3>
-            <hr/>
-            <ResourcesViewTabelCell 
+            <h3> Plugins </h3>
+            <hr />
+            <ResourcesViewTabelCell
                 title={"Import for Figma"}
-                link={"https://www.google.com/"}
+                link={"https://www.figma.com/community/plugin/1143283713928897621"}
                 icon={pluginImage}
-                disabled={true}
             />
-            <ResourcesViewTabelCell 
-                title={"Import for Sketch"}
-                link={"https://www.google.com/"}
-                icon={pluginImage}
-                disabled={true}
-            />
-            <ResourcesViewTabelCell 
-                title={"Import for AdobeXD"}
-                link={"https://www.google.com/"}
-                icon={pluginImage}
-                disabled={true}
 
-            /> */}
-
-            <h3> GitHub </h3> 
-            <hr/>
-            <ResourcesViewTabelCell 
+            <h3> GitHub </h3>
+            <hr />
+            <ResourcesViewTabelCell
                 title={"Genome Web"}
                 link={"https://github.com/caoimghgin/genome"}
                 icon={githubLogoImage}
             />
-            <ResourcesViewTabelCell 
+            <ResourcesViewTabelCell
                 title={"Genome Figma Importer"}
                 link={"https://github.com/caoimghgin/genome-import-figma"}
                 icon={githubLogoImage}
             />
 
             <h3> Contact </h3>
-            <hr/>
-            <ResourcesViewTabelCell 
+            <hr />
+            <ResourcesViewTabelCell
                 title={"LinkedIn"}
                 link={"https://www.linkedin.com/in/kevinrmuldoon/"}
                 icon={linkedInImage}
 
             />
-            <ResourcesViewTabelCell 
+            <ResourcesViewTabelCell
                 title={"Twitter"}
                 link={"https://twitter.com/caoimghgin"}
                 icon={twitterLogoImage}
             />
-            <ResourcesViewTabelCell 
+            <ResourcesViewTabelCell
                 title={"My Portfolio"}
                 link={"https://uxfol.io/kmuldoon"}
                 icon={portfolioImage}
-
             />
-
         </ModalContainer>
     )
 }
@@ -123,20 +96,9 @@ interface IResourcesViewTabelCell {
     action?: (arg: string[]) => void
 }
 
-const ResourcesViewTabelCell: React.FC<IResourcesViewTabelCell> = ({icon, title, subtitle, link, disabled} : IResourcesViewTabelCell) => {
+const ResourcesViewTabelCell: React.FC<IResourcesViewTabelCell> = ({ icon, title, subtitle, link, disabled }: IResourcesViewTabelCell) => {
 
     const [image, setImage] = React.useState(icon)
-
-    useEffect(() => {
-        // initColumnIndex()
-        // let columnIndex = localStorage.getItem('columnIndex') as string
-        // let index = parseInt(columnIndex) + 1
-        // setColumn(columns[index])
-
-        // localStorage.setItem('columnIndex', index.toString())
-        // localStorage.setItem(columns[index], model.semantic)
-
-    }, []);
 
     const View = styled.div`
         display: flex;
@@ -147,7 +109,6 @@ const ResourcesViewTabelCell: React.FC<IResourcesViewTabelCell> = ({icon, title,
     const Content = styled.div`
         display: flex;
         width: 360px;
-        /* background-color: green; */
         align-items: center;
     `
     const LeftAccessory = styled.div`
@@ -156,46 +117,36 @@ const ResourcesViewTabelCell: React.FC<IResourcesViewTabelCell> = ({icon, title,
         justify-content: center;
         align-items: center;
         object-fit: contain;
-
-        /* logoUXCollective */
-        /* background-color: red */
-    `    
+    `
     const RightAccessory = styled.div`
         display: flex;
         justify-content:flex-end; 
         align-items: center;
         width: 100px;
-        /* background-color: blue */
-    `    
+    `
 
     const Button = styled.button`
         height: 20px;
-    `  
+    `
 
-const handleOnClick = (event: React.MouseEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    window.open(link);
-    console.log()
-}
+    const handleOnClick = (event: React.MouseEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        window.open(link);
+        console.log()
+    }
 
     return (
         <View>
             <LeftAccessory>
-            <img src={icon} className="logoUXCollective" alt="logo" />
+                <img src={icon} className="logoUXCollective" alt="logo" />
             </LeftAccessory>
             <Content>
                 <b>{title}</b>
             </Content>
             <RightAccessory>
-
-            <form>
-                <input type="button" value="Open" onClick={handleOnClick} disabled={disabled} /> 
-            </form>
-
-            {/* <button onclick="location.href='http://www.example.com'" type="button">
-         www.example.com</button> */}
-                {/* <a href={(link ? link: "")} target="_parent"><button>Open</button></a> */}
-                {/* <Button>Open</Button> */}
+                <form>
+                    <input type="button" value="Open" onClick={handleOnClick} disabled={disabled} />
+                </form>
             </RightAccessory>
         </View>
     )
